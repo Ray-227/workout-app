ec0bb3c14cb9b6e96ad6b7fd1a74c7b4a170f520
RAY <ray-227@ya.ru>
2022-12-25 21:08:36 +0500

    RAY-23/BACK start back

---
+++ back/src/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
+import colors from 'colors'
+
+
+dotenv.config()
+
+const app = express()
+
+if (process.env.NODE_ENV === 'DEV') {
+  app.use(morgan('dev'))
+}
+
+app.use(express.json())
