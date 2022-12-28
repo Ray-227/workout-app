a2fdcc0f4e1790a64cf9f410f981afd23d6ade7a
RAY <ray-227@ya.ru>
2022-12-28 21:46:54 +0500

    RAY-24/BACK first route and controler

---
+++ back/src/controllers/userController.js
@@ -0,0 +1,6 @@
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+export const getUserProfile = (req, res) => ({
+
+});
--- back/src/server.js
+++ back/src/server.js
@@ -3,6 +3,8 @@ import morgan from 'morgan'
 import dotenv from 'dotenv'
 import colors from 'colors'
 
+import userRoutes from './routes/userRoutes.js';
+
 
 dotenv.config()
 
@@ -13,3 +15,11 @@ if (process.env.NODE_ENV === 'DEV') {
 }
 
 app.use(express.json())
+
+app.use('/api/users', userRoutes)
+
+const PORT = process.env.PORT || 5000
+app.listen(
+  PORT,
+  () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+)
