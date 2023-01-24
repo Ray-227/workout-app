896844a4063c4646cfed35c67e72d5173e3587aa
RAY <ray-227@ya.ru>
2023-01-24 23:28:12 +0500

    RAY-38/BACK completed get profile

--- back/src/controllers/user/profileController.js
+++ back/src/controllers/user/profileController.js
@@ -1,11 +1,42 @@
 import asyncHandler from 'express-async-handler'
 import User from '../../models/userModel.js'
+import ExercisesLog from '../../models/exercisesLogModel.js';
+import WorkoutLog from '../../models/workoutLogModel.js';
 
 
 // @desc Get user profile
 // @route GET /api/users/profile
 // @access Private
 export const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id).select('-password')
-  res.json(user)
+  const user = await User.findById(req.user._id).select('-password').lean()
+
+  const exercisesLogByUser = await ExercisesLog.find({
+    user: user._id,
+    completed: true,
+  })
+
+  let countExercisesTimesCompleted = 0
+  let kgs = 0
+
+  exercisesLogByUser.forEach(log => {
+    countExercisesTimesCompleted += log.times.length
+
+    log.times.forEach(item => {
+      kgs += item.weight
+    })
+  })
+
+  const minutes = Math.ceil(countExercisesTimesCompleted * 2.3)
+
+  const workouts = await WorkoutLog.find({
+    user: user._id,
+    completed: true,
+  }).countDocuments()
+
+  res.json({
+    ...user,
+    minutes,
+    workouts,
+    kgs,
+  })
 });
